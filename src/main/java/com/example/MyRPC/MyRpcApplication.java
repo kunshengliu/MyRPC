fc42c3d1d4187ce300f67fdd5235fb53a5efa3c6
liukunsheng <littlepanger@163.com>
2017-06-25 01:11:07 +0800

    rpc start

---
+++ src/main/java/com/example/MyRPC/MyRpcApplication.java
@@ -0,0 +1,12 @@
+package com.example.MyRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRpcApplication.class, args);
+	}
+}
